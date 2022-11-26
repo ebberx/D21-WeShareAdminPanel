86fb0b45d4f6049937ff06828dca707eb7c6ec01
Ebberx <esben_dahl@hotmail.com>
2022-11-26 23:00:24 +0100

    Bindable template

---
+++ D21WeShareAdminPanel/Model/Bindable.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace NNTPClient.Model
+{
+	public abstract class Bindable : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		protected void propertyIsChanged([CallerMemberName] string memberName = "") {
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(memberName));
+		}
+	}
+}
